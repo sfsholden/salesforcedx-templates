e3604b9425a6da3b6659ebcf8e6b60b8e35da701
Shelby <shelby.holden@salesforce.com>
2020-10-01 16:00:38 -0600

    chore: add version check

--- scripts/publish-workflow.sh
+++ scripts/publish-workflow.sh
@@ -6,6 +6,16 @@
 CircleCIToken=$1
 PublishType=$2
 PublishVersion=$3
+
+if [ ! -z "${PublishVersion}" ]; then
+  if [[ "${PublishVersion}" =~ ^[0-9]{2}\.[0-9]{1,2}\.[0-9]{1,2}$ ]]; then
+    echo "Publish version ${PublishVersion} is valid."
+  else
+    echo "Publish version ${PublishVersion} does not fit the format xx.yy.zz. Exiting."
+    exit 1
+  fi
+fi
+
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "main",
   "parameters": {
