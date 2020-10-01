fb50b56bdea19615be0ac236d94efb78cf5aed6a
Shelby <shelby.holden@salesforce.com>
2020-10-01 15:17:30 -0600

    chore: update to handle a manual override for the release version

--- scripts/publish-workflow.sh
+++ scripts/publish-workflow.sh
@@ -5,13 +5,15 @@
 
 CircleCIToken=$1
 PublishType=$2
+PublishVersion=$3
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "main",
   "parameters": {
     "publish": true,
-    "publish-type": "'"${PublishType}"'"
+    "publish-type": "'"${PublishType}"'",
+    "publish-version": "'"${PublishVersion}"'"
   }
-}' https://circleci.com/api/v2/project/gh/forcedotcom/salesforcedx-templates/pipeline
+}' https://circleci.com/api/v2/project/gh/sfsholden/salesforcedx-templates/pipeline
 
 # open the release pipe line url
-open "https://app.circleci.com/pipelines/github/forcedotcom/salesforcedx-templates"
+open "https://app.circleci.com/pipelines/github/sfsholden/salesforcedx-templates"
