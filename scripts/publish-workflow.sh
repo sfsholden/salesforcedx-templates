a93849761a164d533f858c035b78b73389dd41a8
Shelby Holden <45604118+sfsholden@users.noreply.github.com>
2020-10-01 16:16:49 -0600

    Revert "chore: update to handle a manual override for the release version"

--- scripts/publish-workflow.sh
+++ scripts/publish-workflow.sh
@@ -5,25 +5,13 @@
 
 CircleCIToken=$1
 PublishType=$2
-PublishVersion=$3
-
-if [ ! -z "${PublishVersion}" ]; then
-  if [[ "${PublishVersion}" =~ ^[0-9]{2}\.[0-9]{1,2}\.[0-9]{1,2}$ ]]; then
-    echo "Publish version ${PublishVersion} is valid."
-  else
-    echo "Publish version ${PublishVersion} does not fit the format xx.yy.zz. Exiting."
-    exit 1
-  fi
-fi
-
 curl -v -u ${CircleCIToken}: -X POST --header "Content-Type: application/json" -d '{
   "branch": "main",
   "parameters": {
     "publish": true,
-    "publish-type": "'"${PublishType}"'",
-    "publish-version": "'"${PublishVersion}"'"
+    "publish-type": "'"${PublishType}"'"
   }
-}' https://circleci.com/api/v2/project/gh/sfsholden/salesforcedx-templates/pipeline
+}' https://circleci.com/api/v2/project/gh/forcedotcom/salesforcedx-templates/pipeline
 
 # open the release pipe line url
-open "https://app.circleci.com/pipelines/github/sfsholden/salesforcedx-templates"
+open "https://app.circleci.com/pipelines/github/forcedotcom/salesforcedx-templates"
